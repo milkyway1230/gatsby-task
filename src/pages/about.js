4cc1eda6c16571ac8ade410602ea4ad7de44de74
Yuri <yurylabiak8@gmail.com>
2020-05-24 18:00:13 +0300

    Menu bar is created

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const AboutPage = () => (
+  <div>
+    <h1>About Us</h1>
+    <p>lorem40</p>
+  </div>
+)
+
+export default AboutPage
---
+++ src/pages/services.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const ServicesPage = () => (
+  <div>
+    <h1>Our Services</h1>
+    <p>lorem40</p>
+  </div>
+)
+
+export default ServicesPage
